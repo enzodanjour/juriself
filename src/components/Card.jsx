c135908780d2a94ab2e70b56e4e88d8fa7dda678
Davi Dantas <davi.soares02@gmail.com>
2023-08-28 18:36:11 -0300

    redirecionamento e versão mobile funcionais

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ function Card(props) {
 
     return(
         <>
-        <div className="w-4/12 p-3 border py-6 ">
-            <h1 className="text-xl mb-3 underline ">{props.title}</h1>
+        <div className="lg:w-4/12 p-3 border py-6 mt-2 md:mt-3 ">
+            <h1 className="text-xl mb-3 underline text-sky-700 ">{props.title}</h1>
             <h3>{props.content}</h3>
 
         </div>
